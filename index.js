1e37c9446b2f8f9825cd8de048f596845a581c85
anu-007 <anubhutiv1@gmail.com>
2019-02-24 21:57:12 +0530

    adds token based authorization

--- index.js
+++ index.js
@@ -97,5 +97,6 @@ const unifiedServer = function(req,res) {
 // defining a request router
 const router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens
 }
